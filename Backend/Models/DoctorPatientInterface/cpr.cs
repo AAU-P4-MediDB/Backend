fedf5ae76769f1679bb967769d676b187285fdf5
lilleole <burhennemalte1@gmail.com>
2026-03-27 16:26:05 +0100

    kanban tickets 2008 and 2009

---
+++ Backend/Models/DoctorPatientInterface/cpr.cs
@@ -0,0 +1,5 @@
+﻿namespace Backend.Models;
+public class VitalsFetchingRequest
+{
+  public string CPR_pt { get; set; } = null!;
+}
---
+++ Backend/Models/DoctorPatientInterface/dataupdate.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json;
+namespace Backend.Models;
+
+public class VitalsUpdateRequest
+{
+  public JsonElement vitals { get; set; }
+}
