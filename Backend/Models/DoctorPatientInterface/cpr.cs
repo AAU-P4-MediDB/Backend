edd338cd75d92861db31b44d3093ddf682eb0fc7
lilleole <burhennemalte1@gmail.com>
2026-03-27 16:26:05 +0100

    kanban tickets 2008 and 2009

---
+++ Backend/Models/DoctorPatientInterface/cpr.cs
@@ -0,0 +1,5 @@
+﻿namespace Backend.Models;
+public class ptdataFetchingRequest
+{
+  public string CPR_pt { get; set; } = null!;
+}
---
+++ Backend/Models/DoctorPatientInterface/dataupdate.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+namespace Backend.Models;
+
+public class VitalsUpdateRequest
+{
+  public JsonElement vitals { get; set; }
+}
+
+public class JournalUpdateRequest
+{
+  public JsonElement journal { get; set; }
+}
+public class PrescriptionUpdateRequest
+{
+  public JsonElement prescriptions { get; set; }
+}
+public class DiagnosesUpdateRequest
+{
+  public string diagnoses { get; set; }
+}
+public class AppointmentUpdateRequest
+{
+  public JsonElement appointment { get; set; }
+}
+
+public class LabResultUpdateRequest
+{
+  public JsonElement lab_result { get; set; }
+}
+public class InfoUpdateRequest
+{
+  public int cpr_key { get; set; }
+  public string? name { get; set; }
+  public string? pronouns { get; set; }
+  public int bday { get; set; }
+  public bool bio_sex { get; set; }
+  public string? pfp { get; set; }
+}
+
