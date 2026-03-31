6fa35f7cf1a97ef3ab81633f9eb53a6273fe59a5
lilleole <burhennemalte1@gmail.com>
2026-03-31 10:46:37 +0200

    fixed type mismatch

--- Backend/Models/PatientManagement/PatientRegistration.cs
+++ Backend/Models/PatientManagement/PatientRegistration.cs
@@ -9,7 +9,7 @@ public class PatientRegistrationRequest
   public float weight { get; set; }
   public bool bioGender { get; set; }
   public int cprKey { get; set; }
-  public string[] diagnosis { get; set; } = null!;
+  public List<string>? diagnosis { get; set; } = null!;
   public string vitals { get; set; } = null!;
   public string? prescriptions { get; set; }
   public string? pfp { get; set; }
