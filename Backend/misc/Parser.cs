5b2cac58628cccae6f08dada579de47581f1b45e
lilleole <burhennemalte1@gmail.com>
2026-04-01 09:24:02 +0200

    made a parser class and perm int parser func

---
+++ Backend/misc/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace Backend.Models;
+
+public static class Parser
+{
+  public static bool[] ParsePermInt(int value)
+  {
+    bool[] bits = new BitArray(new[] { value })
+      .Cast<bool>()
+      .Take(16)
+      .ToArray();
+    
+    return bits;
+  }
+}
