db275d63146f3db64332bceabfb08bd6ea0e7188
Voxy <development@voxvoltera.com>
2026-04-27 10:27:47 +0200

    fixed and updated api limits

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -63,6 +63,33 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddControllers();
 builder.Services.AddScoped<TokenService>();
 
+//Global Rate Limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+    {
+        return RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 1000,        // max requests
+                Window = TimeSpan.FromMinutes(1),
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 0
+            });
+    });
+
+    // Specific Rate limit for login
+    options.AddFixedWindowLimiter("login", opt =>
+    {
+        opt.PermitLimit = 100;
+        opt.Window = TimeSpan.FromMinutes(1);
+        opt.QueueLimit = 0;
+    });
+
+    options.RejectionStatusCode = 429;
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -89,33 +116,6 @@ app.Use(async (context, next) =>
 {
     Console.WriteLine($"[{DateTime.Now}] Request: {context.Request.Method} {context.Request.Path}");
     await next();
-});
-
-//Global Rate Limiting
-builder.Services.AddRateLimiter(options =>
-{
-    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
-    {
-        return RateLimitPartition.GetFixedWindowLimiter(
-            partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
-            factory: _ => new FixedWindowRateLimiterOptions
-            {
-                PermitLimit = 100,        // max requests
-                Window = TimeSpan.FromMinutes(1),
-                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
-                QueueLimit = 0
-            });
-    });
-
-    // Specific Rate limit for login
-    options.AddFixedWindowLimiter("login", opt =>
-    {
-        opt.PermitLimit = 5;
-        opt.Window = TimeSpan.FromMinutes(1);
-        opt.QueueLimit = 0;
-    });
-
-    options.RejectionStatusCode = 429;
 });
 
 app.UseAuthentication();   // must be before UseAuthorization
